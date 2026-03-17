87fe971d139cbb4326106725cb75b4ea81cf8b76
Benedict Chinaza <benedictchinaza765@gmail.com>
2026-03-17 21:28:27 +0100

    Revert "Add GitHub Pages production deploy"

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Use relative asset paths so the build works on sub-path hosts
-  // like GitHub Pages (https://user.github.io/repo/).
-  base: './',
 })
